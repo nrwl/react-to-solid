aa207c56f7af6b00979b7e91648d38742d3ef83d
Zack DeRose <zack.derose@gmail.com>
2023-02-09 01:56:27 -0700

    clean up unused generator stuff

--- libs/solid/src/generators/app/generator.ts
+++ libs/solid/src/generators/app/generator.ts
@@ -1,65 +1,10 @@
-import {
-  addProjectConfiguration,
-  formatFiles,
-  generateFiles,
-  getWorkspaceLayout,
-  names,
-  offsetFromRoot,
-  Tree,
-  updateJson,
-} from '@nrwl/devkit';
-import * as path from 'path';
-import { AppGeneratorSchema } from './schema';
+import { formatFiles, names, Tree, updateJson } from '@nrwl/devkit';
+import { Linter } from '@nrwl/linter';
 import {
   applicationGenerator as reactAppGenerator,
   setupTailwindGenerator,
 } from '@nrwl/react';
-import { Linter } from '@nrwl/linter';
-
-interface NormalizedSchema extends AppGeneratorSchema {
-  projectName: string;
-  projectRoot: string;
-  projectDirectory: string;
-  parsedTags: string[];
-}
-
-function normalizeOptions(
-  tree: Tree,
-  options: AppGeneratorSchema
-): NormalizedSchema {
-  const name = names(options.name).fileName;
-  const projectDirectory = options.directory
-    ? `${names(options.directory).fileName}/${name}`
-    : name;
-  const projectName = projectDirectory.replace(new RegExp('/', 'g'), '-');
-  const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${projectDirectory}`;
-  const parsedTags = options.tags
-    ? options.tags.split(',').map((s) => s.trim())
-    : [];
-
-  return {
-    ...options,
-    projectName,
-    projectRoot,
-    projectDirectory,
-    parsedTags,
-  };
-}
-
-function addFiles(tree: Tree, options: NormalizedSchema) {
-  const templateOptions = {
-    ...options,
-    ...names(options.name),
-    offsetFromRoot: offsetFromRoot(options.projectRoot),
-    template: '',
-  };
-  generateFiles(
-    tree,
-    path.join(__dirname, 'files'),
-    options.projectRoot,
-    templateOptions
-  );
-}
+import { AppGeneratorSchema } from './schema';
 
 export default async function (tree: Tree, options: AppGeneratorSchema) {
   await reactAppGenerator(tree, {
